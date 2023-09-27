b60f5cde5316170a4bdf7f666a1f0593816e3f00
Brenda <brendakungu3@gmail.com>
2023-09-27 20:57:22 +0300

    functions done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate!"){
+    console.log(`This Saturday, I want to ${activity}`)
+}
+saturdayFun()
+saturdayFun("bathe my dog!")
+
+
+ function mondayWork(work = "go to the office.") {
+    console.log(`This Monday, I will ${work}`)
+}
+mondayWork("work from home")
+mondayWork()
+
+
+
+function wrapAdjective() {
+    const innerWrap = "special"; 
+        return function () {`You are`} 
+    }
+
+wrapAdjective()
